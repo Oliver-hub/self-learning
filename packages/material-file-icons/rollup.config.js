1d342460803eb10a5030a48b4f1e1d9bd8496bf8
oliver-hub <lizekunisme@gmail.com>
2023-03-07 22:42:45 +0800

    feat: add tsconfig and rollup plugins.

--- packages/material-file-icons/rollup.config.js
+++ packages/material-file-icons/rollup.config.js
@@ -1,1 +1,40 @@
-export default {}
+import typescript from '@rollup/plugin-typescript'
+import ts from 'typescript'
+import del from 'rollup-plugin-delete'
+import terser from '@rollup/plugin-terser'
+
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
+const pkg = require('./package.json')
+
+export default {
+  input: 'libs/index.ts',
+
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.browser,
+      format: 'cjs',
+      plugins: [terser()],
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+
+  plugins: [
+    del({
+      targets: 'dist/*',
+      runOnce: true
+    }),
+
+    typescript({
+      typescript: ts,
+      tsconfig: './tsconfig.json'
+    })
+  ],
+}
