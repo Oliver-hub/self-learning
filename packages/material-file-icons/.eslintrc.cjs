18759669a30cde07a7f20ff100b04964f9a29d61
oliver-hub <lizekunisme@gmail.com>
2023-03-07 22:57:04 +0800

    feat: add eslint config

---
+++ packages/material-file-icons/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: [
+    '@typescript-eslint'
+  ],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true
+  },
+  rules: {
+  }
+}
