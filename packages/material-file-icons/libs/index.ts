3ac38e1cacb822a2b5c54a94106cd7c5a13c7074
xwlizekun <xwlizekun@gfpartner.com.cn>
2023-04-18 11:18:48 +0800

    feat: add getAllIcon methods

--- packages/material-file-icons/libs/index.ts
+++ packages/material-file-icons/libs/index.ts
@@ -2,4 +2,25 @@
 // @ts-ignore
 import iconDefs from 'icon-definitions'
 
-console.log(iconDefs)
+// Icon type
+export interface Icon {
+  name: string;
+  extensions?: string[];
+  files?: string[];
+  svg: string;
+}
+
+const defaultIcon: Icon = iconDefs.defaultIcon;
+export {defaultIcon}
+
+//export function getIcon (filename: string): Icon {
+//
+//}
+
+// 获取全部 icon 数组
+export function getAllIcons (): Array<Icon> {
+  return [
+    defaultIcon,
+    ...iconDefs.icons
+  ]
+}
