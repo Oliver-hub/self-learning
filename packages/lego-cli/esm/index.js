117633b0b941a029200bd59db241d08bca321e42
xwlizekun <xwlizekun@gfpartner.com.cn>
2023-06-27 15:38:49 +0800

    init lego cli project

--- packages/lego-cli/esm/index.js
+++ packages/lego-cli/esm/index.js
@@ -1,3 +1,4 @@
 "use strict";
-console.log('Hello typescript');
+console.log('Hello Lego....');
+// https://juejin.cn/post/7232549203007602744
 //# sourceMappingURL=index.js.map
--- packages/lego-cli/src/index.ts
+++ packages/lego-cli/src/index.ts
@@ -1,1 +1,2 @@
-console.log('Hello typescript');
+console.log('Hello Lego....')
+// https://juejin.cn/post/7232549203007602744
